fc6fbc21ea91ccd622ed3b3d7e49792a4399b653
angel <angel.montecino2201@alumnos.ubiobio.cl>
2026-02-11 20:24:44 +0000

    Reporte de ventas para vendedores

--- frontend/web/views/caja.py
+++ frontend/web/views/caja.py
@@ -191,9 +191,7 @@ def ver_reportes(request):
         if sucursal_id:
             params["sucursal_id"] = sucursal_id
 
-        # Filtros de Seguridad para Vendedor
         if request.session.get("rol") == "VENDEDOR":
-            params["usuario_id"] = request.session.get("id_usuario")
             params["sucursal_id"] = request.session.get("id_sucursal") # Fuerza su sucursal
 
         resp = httpx.get(f"{BACKEND_URL}/caja/reportes", params=params, headers=headers)
